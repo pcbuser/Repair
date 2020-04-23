c0f778be8f5f75be9211a4484bf67f2bab7c330c
pcbuser <dtstart2020!>
2020-04-23 11:26:48 +0900

    update

--- src/main/java/carrepair/RepairProcessingController.java
+++ src/main/java/carrepair/RepairProcessingController.java
@@ -13,7 +13,7 @@ import java.util.List;
   RepairProcessingRepository repairProcessingRepository;
 
   //@RequestMapping(method= RequestMethod.PATCH, path="/repairProcessings")
-  @RequestMapping(method= RequestMethod.PATCH, path="/cancel")
+  @RequestMapping(method= RequestMethod.PATCH, path="/repairProcessings")
   public void repairCancel(@RequestBody  RepairProcessing repairProcessing) {
 
    //System.out.println("log : "+repairProcessing.getStatus());
